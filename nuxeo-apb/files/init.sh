4493f3b1f91bb13edbb4247f3c3f2d19edaa6cfe
Damien Metzler <dmetzler@nuxeo.com>
2018-05-18 12:06:02 -0700

    NXCT-44: Add config map provisioning

---
+++ nuxeo-apb/files/init.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+if [ ! -f $NUXEO_DATA/instance.clid -a -f /opt/nuxeo/connect/connect.properties ]; then
+  . /opt/nuxeo/connect/connect.properties
+  if [ -n "$NUXEO_CONNECT_USERNAME" -a -n "$NUXEO_CONNECT_PASSWORD" -a -n "$NUXEO_STUDIO_PROJECT" ]; then  
+    echo "---> Registering instance on connect"
+    nuxeoctl register $NUXEO_CONNECT_USERNAME $NUXEO_STUDIO_PROJECT dev openshift $NUXEO_CONNECT_PASSWORD
+  fi
+fi
