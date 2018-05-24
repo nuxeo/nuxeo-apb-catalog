b9eccc6ff497357472c0293aa742b11a59c5cd14
Damien Metzler <dmetzler@nuxeo.com>
2018-05-24 11:17:12 -0700

    NXCT-44: add worker queues configuration

--- nuxeo-apb/files/init.sh
+++ nuxeo-apb/files/init.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
+
+# Use the log4j we get from the config.
 cp -f /docker-entrypoint-initnuxeo.d/log4j.xml $NUXEO_HOME/lib/log4j.xml
+
+
+# Configure queues handling
+QUEUES_CONFIG=/docker-entrypoint-initnuxeo.d/interactive-queues-config.xml
+if [ $IS_WORKER == "1" ]; then
+	QUEUES_CONFIG=/docker-entrypoint-initnuxeo.d/worker-queues-config.xml
+fi
+cp $QUEUES_CONFIG /opt/nuxeo/server/templates/common/config
+
 
 if [ ! -f $NUXEO_DATA/instance.clid -a -f /opt/nuxeo/connect/connect.properties ]; then
   . /opt/nuxeo/connect/connect.properties
