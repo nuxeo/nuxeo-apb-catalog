48b62f7820b60ef960dedd91c2b865da664e009a
Damien Metzler <dmetzler@nuxeo.com>
2018-06-10 08:58:32 +0200

    NXCT-42: generate certs and add verifications

--- nuxeo-kafka-apb/files/init-certs.sh
+++ nuxeo-kafka-apb/files/init-certs.sh
@@ -7,10 +7,10 @@ hostname=$(hostname)
 # Generate the ca cert
 ca_crt=/caroot/tls.crt
 ca_key=/caroot/tls.key
-conf_dir=/etc/kafka/secrets
+conf_dir=/work-dir
 
-key_store=$conf_dir/keystore.p12
-trust_store=$conf_dir/truststore.p12
+key_store=$conf_dir/keystore.jks
+trust_store=$conf_dir/truststore.jks
 
 rm -f $key_store
 rm -f $trust_store
@@ -19,44 +19,47 @@ rm -f $trust_store
 pushd $conf_dir
 
 
-# Create Truststore
-keytool -keystore $trust_store -storetype pkcs12 -noprompt -alias CARoot -import -file $ca_crt -storepass changeit
+# Create Truststor
+
+keytool -keystore $trust_store  -noprompt -alias CARoot -import -file $ca_crt -storepass changeit
 
 # Generate a key in the keystore
 keytool -genkey -noprompt \
                  -alias ${hostname} \
                  -dname "CN=${hostname}.${DN_SUFFIX}" \
                  -keystore $key_store \
-                 -keyalg RSA \
-                 -storetype pkcs12 \
+                 -keyalg RSA  \
+                 -keypass changeit \
                  -storepass changeit
 
 # Create CSR, sign the key and import back into keystore
 keytool -keystore $key_store \
            -alias ${hostname} \
            -certreq \
-           -file key.csr \
-           -storetype pkcs12 \
+           -file key.csr  \
+           -keypass changeit \
            -storepass changeit
 
 # Sign CSR
-openssl x509 -req -CA $ca_crt -CAkey $ca_key -in key.csr -out key.crt -days 3650 -CAcreateserial -CAserial /tmp/tls.srl -extensions server_ext
+openssl x509 -req -CA $ca_crt -CAkey $ca_key -in key.csr -out key.crt -days 3650 -CAcreateserial -CAserial /tmp/tls.srl -extensions server_ext -passin pass:changeit
 
-# Import CA cert in keystore
-keytool -keystore $key_store -storetype pkcs12  --noprompt -alias CARoot -import -file $ca_crt -storepass changeit
+# Import CA cert in keystor
+keytool -keystore $key_store --noprompt -alias CARoot -import -file $ca_crt -storepass changeit
 
 # Import signed key  cert in keystore
 keytool -keystore $key_store --noprompt \
-    -alias ${hostname} \
-    -storetype pkcs12 \
+    -alias ${hostname}  \
     -import \
     -file key.crt \
+    -keypass changeit \
     -storepass changeit
 
 rm -f key.csr
 rm -f key.crt
 
-
+cp /tmp/jaas/jaas.conf $conf_dir
+echo "changeit" > $conf_dir/credentials
+ls -la $conf_dir
 popd
 
 
