d054f2acf1c0cfd63dde0416419a3e0cbcefdf0c
Damien Metzler <dmetzler@nuxeo.com>
2020-12-17 14:24:17 +0100

    NXCT-459: adapt deployment for Nuxeo 11 and enhance automation

--- nuxeo-apb/files/init.sh
+++ nuxeo-apb/files/init.sh
@@ -119,23 +119,6 @@ EOT
 fi
 
 
-# Configure queues handling
-QUEUES_CONFIG=/docker-entrypoint-initnuxeo.d/interactive-queues-config.xml
-if [ "$IS_WORKER" == "1" ]; then
-	QUEUES_CONFIG=/docker-entrypoint-initnuxeo.d/worker-queues-config.xml
-fi
-cp $QUEUES_CONFIG /opt/nuxeo/server/templates/common/config/queues-config.xml
-
-
-if [ ! -f $NUXEO_DATA/instance.clid -a -f /opt/nuxeo/connect/connect.properties ]; then
-  . /opt/nuxeo/connect/connect.properties
-  if [ -n "$NUXEO_CONNECT_USERNAME" -a -n "$NUXEO_CONNECT_PASSWORD" -a -n "$NUXEO_STUDIO_PROJECT" ]; then
-    echo "---> Registering instance on connect"
-    nuxeoctl register $NUXEO_CONNECT_USERNAME $NUXEO_STUDIO_PROJECT dev openshift $NUXEO_CONNECT_PASSWORD
-  fi
-fi
-
-
 if [ -f $NUXEO_DATA/instance.clid ]  && [ ${NUXEO_INSTALL_HOTFIX:='true'} == "true" ]; then
 	echo "---> Installing hotfixes"
 	nuxeoctl mp-hotfix
