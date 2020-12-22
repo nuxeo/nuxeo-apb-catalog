63b3cec7c526279c5b85b1d70af3767d33a8267f
Damien Metzler <dmetzler@nuxeo.com>
2020-12-22 15:27:45 +0100

    NXCT-459: reinstate registration on Connect for 10.10 sandboxes

--- nuxeo-apb/files/init.sh
+++ nuxeo-apb/files/init.sh
@@ -119,6 +119,15 @@ EOT
 fi
 
 
+if [ ! -f $NUXEO_DATA/instance.clid -a -f /opt/nuxeo/connect/connect.properties ]; then
+  . /opt/nuxeo/connect/connect.properties
+  if [ -n "$NUXEO_CONNECT_USERNAME" -a -n "$NUXEO_CONNECT_PASSWORD" -a -n "$NUXEO_STUDIO_PROJECT" ]; then
+    echo "---> Registering instance on connect"
+    nuxeoctl register $NUXEO_CONNECT_USERNAME $NUXEO_STUDIO_PROJECT dev openshift $NUXEO_CONNECT_PASSWORD
+  fi
+fi
+
+
 if [ -f $NUXEO_DATA/instance.clid ]  && [ ${NUXEO_INSTALL_HOTFIX:='true'} == "true" ]; then
 	echo "---> Installing hotfixes"
 	nuxeoctl mp-hotfix
